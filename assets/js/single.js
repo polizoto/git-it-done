398fcd215b0b62f2d03d9a72d29c803b09bd467d
polizoto <polizoto@hotmail.com>
2021-12-14 21:27:02 -0500

    Added safeguards to bring user back to main page if query yields zero results

--- assets/js/single.js
+++ assets/js/single.js
@@ -7,8 +7,13 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName)
-    repoNameEl.textContent = repoName;
+    if(repoName) {
+      repoNameEl.textContent = repoName;
+      getRepoIssues(repoName);
+    }
+    else {
+      document.location.replace("./index.html");
+    }
 }
 
 var displayWarning = function(repo) {
@@ -74,7 +79,7 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html");
         }
       });
   };
